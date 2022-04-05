73d3d1841504f9c7012560fcb585acdb196c3d1a
LutSerg <asteroster@gmail.com>
2022-04-05 23:06:14 +0300

    filling date of birth

--- src/test/java/com/demoqa/webform/RegistrationFormTests.java
+++ src/test/java/com/demoqa/webform/RegistrationFormTests.java
@@ -1,11 +1,12 @@
 package com.demoqa.webform;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.support.ui.Select;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class RegistrationFormTests {
 
@@ -31,6 +32,25 @@ public class RegistrationFormTests {
 
             //выделение радиобаттона Пол
             $(".custom-control-label[for=gender-radio-2]").click();
+
+            //вставка номера телефона
+            $("#userNumber").setValue("9552231745");
+
+            //заполнение даты рождения
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").click();
+            $(".react-datepicker__month-select").selectOption("May");
+            $(".react-datepicker__year-select").click();
+            $(".react-datepicker__year-select").selectOption("1999");
+            $(".react-datepicker__year-select").click();
+            $(".react-datepicker__week", 2).click();
+
+
+
+
+
+
+
 
 
 
