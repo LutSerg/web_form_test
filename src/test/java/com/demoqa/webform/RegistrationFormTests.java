8a2219a29304ca39b534f73922c0fc4f24580bf6
LutSerg <asteroster@gmail.com>
2022-04-06 00:56:56 +0300

    ended filling

--- src/test/java/com/demoqa/webform/RegistrationFormTests.java
+++ src/test/java/com/demoqa/webform/RegistrationFormTests.java
@@ -1,5 +1,6 @@
 package com.demoqa.webform;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.support.ui.Select;
 import org.junit.jupiter.api.BeforeAll;
@@ -21,6 +22,7 @@ public class RegistrationFormTests {
             String name = "Serg";
             String surname = "erweFF";
             String email = "eFdd@aadasd.gg";
+            String adress = "test address 19";
 
             open ("/automation-practice-form");
             //вставка имени и фамилии в полях firstName и lastName
@@ -45,14 +47,27 @@ public class RegistrationFormTests {
             $(".react-datepicker__year-select").click();
             $(".react-datepicker__week", 2).click();
 
+            //Выбор хобби
+            $(".custom-control-label[for=hobbies-checkbox-1]").click();
 
+            //загрузка изображения
+            $("#uploadPicture").uploadFromClasspath("getting_strt.PNG");
 
+           // Выбор предмета
+            $("#subjectsInput").setValue("h");
+            $$("[id^='react-select-2-option']").find(Condition.text("History")).click();
 
+            //добавление адреса
+            $(".form-control#currentAddress").setValue(adress);
 
+            //Выбор State and City
+            $(".css-1wa3eu0-placeholder").click();
+            $("#react-select-3-option-0").click();
+            $(".css-1wa3eu0-placeholder").click();
+            $("#react-select-4-option-1").click();
 
-
-
-
+            //нажатие Submit
+            $("#submit").click();
 
         }
 }
