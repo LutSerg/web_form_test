82c841971a6e760b713caaf909f728bb3c72a5f4
LutSerg <asteroster@gmail.com>
2022-04-12 20:51:46 +0300

    added some vars

--- src/test/java/com/demoqa/webform/RegistrationFormTests.java
+++ src/test/java/com/demoqa/webform/RegistrationFormTests.java
@@ -9,7 +9,9 @@ import org.openqa.selenium.support.ui.Select;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
 
 
 public class RegistrationFormTests {
@@ -29,16 +31,21 @@ public class RegistrationFormTests {
     }
         @Test
         void fillRegistrationForm() {
-            String name = "Serg";
-            String surname = "erweFF";
-            String email = "eFdd@aadasd.gg";
-            String adress = "test address 19";
-            String phone = "9552231745";
+            String name = "Serg",
+                    surname = "erweFF",
+                    fullName  = format("%s %s", name, surname),
+                    email = "eFdd@aadasd.gg",
+                    adress = "test address 19",
+                    phone = "9552231745";
             SelenideElement assertVar = $(".table-responsive");
             SelenideElement month = $(".react-datepicker__month-select");
             SelenideElement year = $(".react-datepicker__year-select");
 
             open ("/automation-practice-form");
+            //убираем баннеры
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+
             //вставка имени и фамилии в полях firstName и lastName
             $("#firstName").setValue(name);
             $("#lastName").setValue(surname);
@@ -47,7 +54,9 @@ public class RegistrationFormTests {
             $("#userEmail").setValue(email);
 
             //выделение радиобаттона Пол
-            $(".custom-control-label[for=gender-radio-2]").click();
+            // $(".custom-control-label[for=gender-radio-3]").click();
+            //$("#gender-radio-2").parent().click();
+            $("#genterWrapper").$(byText("Female")).click();
 
             //вставка номера телефона
             $("#userNumber").setValue(phone);
@@ -59,11 +68,13 @@ public class RegistrationFormTests {
             year.click();
             year.selectOption("1999");
             year.click();
-            $(".react-datepicker__week", 2).click();
+           // $(".react-datepicker__week", 2).click();
+            $(".react-datepicker__day--027:not(.react-datepicker__day--outside-month)").click();
 
 
             //Выбор хобби
-            $(".custom-control-label[for=hobbies-checkbox-1]").click();
+            //$(".custom-control-label[for=hobbies-checkbox-1]").click();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
 
             //загрузка изображения
             $("#uploadPicture").uploadFromClasspath("getting_strt.PNG");
@@ -76,21 +87,27 @@ public class RegistrationFormTests {
             $(".form-control#currentAddress").setValue(adress);
 
             //Выбор State and City
+            /*
             $(".css-1wa3eu0-placeholder").click();
             $("#react-select-3-option-0").click();
             $(".css-1wa3eu0-placeholder").click();
             $("#react-select-4-option-1").click();
+            */
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Gurgaon")).click();
 
             //нажатие Submit
             $("#submit").click();
 
 
             //Asserts
-            assertVar.shouldHave(Condition.text("Serg erweFF"));
+            assertVar.shouldHave(Condition.text(fullName));
             assertVar.shouldHave(Condition.text(email));
             assertVar.shouldHave(Condition.text("Female"));
             assertVar.shouldHave(Condition.text(phone));
-            assertVar.shouldHave(Condition.text("12 May,1999"));
+            assertVar.shouldHave(Condition.text("27 May,1999"));
             assertVar.shouldHave(Condition.text("History"));
             assertVar.shouldHave(Condition.text("Sports"));
             assertVar.shouldHave(Condition.text("getting_strt.PNG"));
